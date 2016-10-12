930a2165e0fc8f4f07b83b290ca90087eef3a1ab
anjunqing <738418625@qq.com>
2016-10-12 20:41:24 +0800

    456

--- static/build/.module-cache/537e931cf2890e618d891bc26f7c45d391f2d23a.js
+++
@@ -1,9 +0,0 @@
-// React.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('box1')
-// );
-
-ReactDOM.render(
-  React.createElement('h1', null, 'Hello, world!'),
-  document.getElementById('box1')  
-);
